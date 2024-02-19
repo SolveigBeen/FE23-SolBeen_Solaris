30e161bf1d3d08a4dfe71782d66efe9a6ed7a06e
SolveigBeen <132928451+SolveigBeen@users.noreply.github.com>
2024-02-19 15:50:12 +0100

    search-planet from input added

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+"use strict";
+/*variabel deklaration */
+const planetSearchName = document.getElementById("planetSearchName");
+const searchBtn = document.getElementById("search-btn");
 
+searchBtn.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent default form submission behavior
+  let searchPlanet=planetSearchName.value;
+  console.log(searchPlanet);
+  clearInput()
+});
+
+/* Nollställer och förbereder drop-down för ny sökning*/
+function clearInput() {
+  planetSearchName.value = '';
+}
 
